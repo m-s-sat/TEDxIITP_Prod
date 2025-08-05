56e9b87cf44a5dde7faefbe06c9e7cb084069d8b
“NihaR_RD” <niharranjandas008@gmail.com>
2025-08-05 19:25:41 +0530

    before integrating razorpay

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 require("dotenv").config();
+const cors = require("cors");
+
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8000;
@@ -6,14 +8,25 @@ const connectDB = require("./connection");
 const contactUsRoutes = require("./routes/contactUs");
 const merchRoutes = require("./routes/merch");
 const ticketsRoutes = require("./routes/tickets");
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelistedDomains.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+//const whitelistedDomains = process.env.WHITELIST_DOMAINS.split(",");
+app.use(cors(corsOptions));
 connectDB();
 
 app.use("/api/contactUs", contactUsRoutes);
 app.use("/api/merch", merchRoutes);
 app.use("/api/tickets", ticketsRoutes);
+//app.use("buyMerch");
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
